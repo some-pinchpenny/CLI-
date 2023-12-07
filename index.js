34c9474a6b6e33b0743e90d525148a81e431c44d
Yuliia Stepanovych <ylkatsr@gmail.com>
2023-12-07 17:06:02 +0100

    adds tasks

---
+++ index.js
@@ -0,0 +1,41 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const contact = await contacts.getContactById(id);
+      console.log(contact);
+      break;
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.log(newContact);
+      break;
+    case "remove":
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+      break;
+  }
+};
+
+invokeAction(argv);
